4bd9c1a905d85f96378f991b893ad6baf8892df7
Latisha Joe <latisha_joe@yahoo.com>
2020-09-28 19:05:50 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,15 @@
-class Backer 
+class Backer
 
-  attr_reader :name, :backed_projects 
+  attr_reader :name, :backed_projects
 
   def initialize(name)
-    @name = name 
+    @name = name
     @backed_projects = []
-  end 
+  end
 
   def backed_project(project)
-    @backed_projects << project 
-    project.backers << self 
+    @backed_projects << project
+    project.backers << self
 
 
 end 
